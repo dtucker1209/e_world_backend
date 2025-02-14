e14d3c8846e851ec49182089a3543793d2f0b17c
D-Money Tucker <d-moneytucker@D-Moneys-MacBook-Pro.local>
2025-02-13 20:28:10 -0600

    api/item

--- server/api/item.js
+++ server/api/item.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-const { prisma } = require("../db");
+const {PrismaClient} = require("@prisma/client");
+const prisma = new PrismaClient();
+const db = require("../db");
 
 // router.use((req, res, next) => {
 //     if (!req.user) {
@@ -11,98 +13,100 @@ const { prisma } = require("../db");
 /* Get all items */
 
 router.get("/", async (req, res, next) => {
-    try {
-        const items = await prisma.Item.findMany({
-            include: {
-                review: true
-            }
-        });
-
-        res.send(items);
-    } catch (error) {
-        next(error);
+  try {
+    
+    const items = await prisma.item.findMany();
+    if (items.length === 0) {
+      console.log("no items found in the table");
+    } else {
+      console.log("items found", items);
     }
+    res.send(items);
+  } catch (error) {
+    console.error("problem fetching items", error);
+    next(error);
+  }
 });
 
 /* Get item by id */
 
 router.get("/:id", async (req, res, next) => {
-    try {
-      const item = await prisma.Item.findUnique({
-        where: {
-          id: Number(req.params.id)
-        }
-      })
-      if (!item) {
-        return res.status(404).send("Item not found.");
-      }
-  
-      res.send(item);
-    } catch (error) {
-      next(error);
+  try {
+    const item = await prisma.Item.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    if (!item) {
+      return res.status(404).send("Item not found.");
     }
-  });
 
-  /* Create an item */
+    res.send(item);
+  } catch (error) {
+    next(error);
+  }
+});
 
-  router.post("/", async (req, res, next) => {
-    try {
-        const item = await prisma.Item.create({
-            // // where: {
-            // //     id: req.params.id
-            // },
-            data: {
-                name: req.body.name,
-                description: req.body.description
-            }
-        })
-        if (!item) {
-            return res.status(404).send("Item not found");
-        }
-        res.send(item);
-    } catch (error) {
-        next(error);
+/* Create an item */
+
+router.post("/", async (req, res, next) => {
+  try {
+    const item = await prisma.Item.create({
+      // // where: {
+      // //     id: req.params.id
+      // },
+      data: {
+        name: req.body.name,
+        description: req.body.description,
+      },
+    });
+    if (!item) {
+      return res.status(404).send("Item not found");
     }
-  });
+    res.send(item);
+  } catch (error) {
+    next(error);
+  }
+});
 
-  /* Update an item */
+/* Update an item */
 
-  router.put("/:id", async (req, res, next) => {
-    try {
-        const item = await prisma.Item.update({
-            where: {
-                id: Number(req.params.id)
-            },
-            data: {
-                name: req.body.name,
-                description: req.body.description
-            }
-        })
-        if (!item) {
-            return res.status(404).send("Item not found")
-        }
-        res.send(item);
-    } catch (error){
-        next(error);
+router.put("/:id", async (req, res, next) => {
+  try {
+    const item = await prisma.Item.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        name: req.body.name,
+        description: req.body.description,
+      },
+    });
+    if (!item) {
+      return res.status(404).send("Item not found");
     }
-  });
+    res.send(item);
+  } catch (error) {
+    next(error);
+  }
+});
 
-  /* Delete an item */
+/* Delete an item */
 
-  router.delete("/:id", async (req, res, next) => {
-    try {
-        const item = await prisma.Item.delete({
-            where: {
-                id: Number(req.params.id)
-            }
-        })
-        if (!item) {
-            return res.status(404).send("Item not found");
-        }
-        res.send(item);
-    } catch (error) {
-        next(error);
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const item = await prisma.Item.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    if (!item) {
+      return res.status(404).send("Item not found");
     }
-  });
+    res.send(item);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
