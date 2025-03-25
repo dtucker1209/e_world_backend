cc45bb8c60bdd6a55e366998bc0acee590102299
D-Money Tucker <d-moneytucker@D-Moneys-MacBook-Pro.local>
2025-03-24 23:07:54 -0500

    added nelify link

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "..", "client/dist")));
 app.use(function(req,res,next){
   res.header(
     "Access-Control-Allow-Origin",
-    "http://localhost:5173"
+    "https://steady-fudge-0afe83.netlify.app"
   );
   res.header(
     "Access-Control-Allow-Headers",
